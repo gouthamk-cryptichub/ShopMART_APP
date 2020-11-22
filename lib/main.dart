35f489b2a152881e56490eafb4d629f5e7873414
Goutham K <k.goutham2000@gmail.com>
2020-11-22 16:57:14 +0530

    implemented Sate-management (use of Providers)

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './screens/products_overview_screen.dart';
+import './screens/product_detail_screen.dart';
+import './providers/products_provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'ShopMART',
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('ShopMART'),
-      ),
-      body: Center(
-        child: Text('Initial Commit'),
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'ShopMART',
+        theme: ThemeData(primarySwatch: Colors.orange, fontFamily: 'Lato'),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (_) => ProductDetailScreen(),
+        },
       ),
     );
   }
---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/products_grid.dart';
+
+class ProductsOverviewScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'ShopMART',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: ProductsGrid(),
+    );
+  }
+}
