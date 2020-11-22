faa72b77e019286f3bb2a2186e10e9562ac2391d
Goutham K <k.goutham2000@gmail.com>
2020-11-22 17:29:15 +0530

    use of providers (lisistning in product_detail_screen.dart)

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products_provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   // final String title;
@@ -11,10 +14,11 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final id = ModalRoute.of(context).settings.arguments as String;
+    final loadedProduct = Provider.of<Products>(context).items.firstWhere((prod) => prod.id == id);
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('title'),
+        title: Text(loadedProduct.title),
       ),
     );
   }
